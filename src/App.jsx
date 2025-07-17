a8655859a13fe608e4d69315d9787736a837e02c
Siddhesh30 <sidspalimkar07@gmail.com>
2025-07-17 23:13:51 +0530

    Adding Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import gsap from 'gsap';
+import gsap from "gsap";
 
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-
 const App = () => {
-  return <div className="flex-center h-[100vh]">
-    <h1 className="text-3xl text-indigo-500 font-bold ">Hello World!</h1>
-  </div>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
+  );
 };
 
 export default App;
